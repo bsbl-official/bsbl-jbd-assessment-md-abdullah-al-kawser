e65f2134fbddcd31db3e25c93a7bdebccd326d03
Kawser <Kawser@DESKTOP-QM1191J>
2026-07-09 17:27:58 +0600

    Created student entity

---
+++ ems/src/main/java/entity/Student.java
@@ -0,0 +1,43 @@
+package entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    @NotBlank
+    private String studentId;
+
+    @NotBlank
+    private String fullName;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String phone;
+
+    @NotBlank
+    private String department;
+
+    @NotNull
+    private LocalDate admissionDate;
+
+}
