9ded42f793f866a23c9756d2ac7fddb63e9c3741
Kawser <Kawser@DESKTOP-QM1191J>
2026-07-09 17:30:22 +0600

    added courses and enrollment entity

--- ems/src/main/java/entity/Enrollment.java
+++ ems/src/main/java/entity/Enrollment.java
@@ -1,5 +1,34 @@
 package entity;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name="enrollments")
 public class Enrollment {
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="student_id")
+    private Student student;
+
+    @ManyToOne
+    @JoinColumn(name="course_id")
+    private Course course;
+
+    @NotBlank
+    private String semester;
+
+    private LocalDate enrollmentDate;
 }
